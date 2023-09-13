3adbc19e6fe5fd9c29c2c01f914f12f2e51fbd74
nazarchykur <nazarchykur@gmail.com>
2023-09-13 15:33:35 +0300

    added an example of more reusability with Children Prop

--- src/App.js
+++ src/App.js
@@ -31,9 +31,7 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrev}>
@@ -61,6 +59,15 @@ export default function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
